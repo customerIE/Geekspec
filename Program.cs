3d584cf5287f61e153a54881e5435a32015e8320
Ilya Shapovalov <customerIE@mail.ru>
2022-09-29 09:13:46 +0300

    Программа решения задачи

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string [] array1 = new string[] {"2","hello","git","name","756"};
+string [] array2 = new string[5];
+int index = 0;
+//Console.WriteLine("Введите элементы массива: ");
+for(int i=0; i<5; i++)
+{
+      if(array1[i].Length<=3)
+        {
+            array2[index]=array1[i];
+            index+=1;
+        }
+}
+for(int i=0; i<5; i++)
+{
+    Console.Write(" " + array2[i]);
+}
